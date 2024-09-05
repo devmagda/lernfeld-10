53b02a17a9fe1ea0a002514fdc5b96858a86a852
devmagda <113769198+devmagda@users.noreply.github.com>
2024-09-05 08:14:12 +0200

    Initial Project
    Project was generated using spring initlializr

---
+++ src/main/java/edu/school/unesco/UnescoWebProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.school.unesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnescoWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnescoWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/school/unesco/UnescoWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.school.unesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnescoWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
